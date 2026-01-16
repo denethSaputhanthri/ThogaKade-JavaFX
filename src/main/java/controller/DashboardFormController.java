1e44bb75af6b965450c7bf138cb6900be6144a30
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-16 23:57:03 +0530

    complete DashboardForm

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class DashboardFormController {
@@ -54,5 +53,4 @@ public class DashboardFormController {
         stage.setTitle("Oder.lk");
         stage.show();
     }
-
 }
