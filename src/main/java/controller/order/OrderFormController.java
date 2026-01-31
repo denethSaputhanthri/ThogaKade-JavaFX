c1829678e3717bae68fd22e7ae0b85b6c71f8dbd
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-31 21:58:10 +0530

    feat: orderFromController and UI customize

--- src/main/java/controller/order/OrderFormController.java
+++ src/main/java/controller/order/OrderFormController.java
@@ -1,4 +1,48 @@
 package controller.order;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class OrderFormController {
+
+    @FXML
+    private TableColumn<?, ?> colCustomerId;
+
+    @FXML
+    private TableColumn<?, ?> colOrderDate;
+
+    @FXML
+    private TableColumn<?, ?> colOrderId;
+
+    @FXML
+    private DatePicker dpOrderDate;
+
+    @FXML
+    private TableView<?> tblOrderInfo;
+
+    @FXML
+    private JFXTextField txtCustomerId;
+
+    @FXML
+    private JFXTextField txtOrderId;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
