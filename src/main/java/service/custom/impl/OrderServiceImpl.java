ffb4da0e31615b1a1bb62fbc550df519cb036577
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-14 02:05:06 +0530

    feat: change architecture layered order feature

--- src/main/java/service/custom/impl/OrderServiceImpl.java
+++ src/main/java/service/custom/impl/OrderServiceImpl.java
@@ -1,24 +1,27 @@
 package service.custom.impl;
 
 import model.dto.Order;
+import repository.RepositoryFactory;
+import repository.custom.OrderRepository;
 import service.custom.OrderService;
+import util.RepositoryType;
 
 import java.util.List;
 
 public class OrderServiceImpl implements OrderService {
-
+    OrderRepository repository= RepositoryFactory.getInstance().getRepositoryType(RepositoryType.ORDER);
     @Override
     public List<Order> getAllOrders() {
-        return List.of();
+        return repository.getAll();
     }
 
     @Override
     public Boolean updateOrder(Order order) {
-        return null;
+        return repository.update(order);
     }
 
     @Override
     public Boolean deleteOrder(String orderId) {
-        return null;
+        return repository.deleteById(orderId);
     }
 }
