7e6c5248ec647d41a5615e7730820f612a96666c
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-16 23:57:17 +0530

    feat: used crud repository method to execute Item getall,update,create,delete SQL quarys

--- src/main/java/repository/custom/impl/ItemRepositoryImpl.java
+++ src/main/java/repository/custom/impl/ItemRepositoryImpl.java
@@ -1,51 +1,53 @@
 package repository.custom.impl;
 
 import db.DBConnection;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import model.dto.Item;
 import repository.custom.ItemRepository;
+import util.CrudUtil;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemRepositoryImpl implements ItemRepository {
 
     @Override
     public List<Item> getAll() {
-        ObservableList<Item> itemDTOS= FXCollections.observableArrayList();
         try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from item");
-            while (resultSet.next()){
-                Item itemDTO=new Item(
+            ResultSet resultSet = CrudUtil.execute("select * from item");
+
+            ArrayList<Item> itemArrayList = new ArrayList<>();
+
+            while (resultSet.next()) {
+                itemArrayList.add(new Item(
                         resultSet.getString("ItemCode"),
                         resultSet.getString("Description"),
                         resultSet.getString("PackSize"),
                         resultSet.getDouble("UnitPrice"),
                         resultSet.getInt("QtyOnHand")
-                );
-                itemDTOS.add(itemDTO);
+                        ));
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return itemDTOS;
+            return itemArrayList;
+    } catch(
+    SQLException e)
 
+    {
+        throw new RuntimeException(e);
     }
+}
+
 
     @Override
     public Boolean create(Item item) {
         try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into item values(?,?,?,?,?)" );
-            preparedStatement.setObject(1,item.getItemCode());
-            preparedStatement.setObject(2,item.getDescription());
-            preparedStatement.setObject(3,item.getPackSize());
-            preparedStatement.setObject(4,item.getUnitPrice());
-            preparedStatement.setObject(5,item.getQtyOnHand());
-            return preparedStatement.executeUpdate()>0;
+            return CrudUtil.execute("insert into item values(?,?,?,?,?)",
+                    item.getItemCode(),
+                    item.getDescription(),
+                    item.getPackSize(),
+                    item.getUnitPrice(),
+                    item.getQtyOnHand()
+            );
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -54,14 +56,14 @@ public class ItemRepositoryImpl implements ItemRepository {
     @Override
     public Boolean update(Item item) {
         try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("update item set Description=?,PackSize=?,UnitPrice=?,QtyOnHand=? where ItemCode=?");
-            preparedStatement.setObject(1,item.getDescription());
-            preparedStatement.setObject(2,item.getPackSize());
-            preparedStatement.setObject(3,item.getUnitPrice());
-            preparedStatement.setObject(4,item.getQtyOnHand());
-            preparedStatement.setObject(5,item.getItemCode());
-            return preparedStatement.executeUpdate()>0;
+            return CrudUtil.execute("update item set Description=?,PackSize=?,UnitPrice=?,QtyOnHand=? where ItemCode=?",
+                    item.getDescription(),
+                    item.getPackSize(),
+                    item.getUnitPrice(),
+                    item.getQtyOnHand(),
+                    item.getItemCode()
+            );
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -70,10 +72,7 @@ public class ItemRepositoryImpl implements ItemRepository {
     @Override
     public Boolean deleteById(String itemCode) {
         try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("delete from item where ItemCode=?");
-            preparedStatement.setObject(1,itemCode);
-            return preparedStatement.executeUpdate()>0;
+            return CrudUtil.execute("delete from item where ItemCode=?", itemCode);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
