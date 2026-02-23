2ea1b7abda5ecc236a12627bd8bd19a49234c6d4
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-23 18:50:20 +0530

    feat: customize ui

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -1,20 +1,39 @@
 package controller;
 
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
+import javafx.scene.control.Label;
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class DashboardFormController {
+public class DashboardFormController implements Initializable {
     @FXML
     private AnchorPane dashRoot;
+
+    @FXML
+    private AnchorPane dashRoot1;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
 
     Stage stage =new Stage();
 
@@ -73,4 +92,25 @@ public class DashboardFormController {
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        localTime();
+    }
+
+    private void localTime(){
+         Date date =new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        lblDate.setText(simpleDateFormat.format(date));
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, actionEvent -> {
+            LocalTime now = LocalTime.now();
+            lblTime.setText(now.getHour() + ":" + now.getMinute() + ":" + now.getSecond());
+        }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
 }
